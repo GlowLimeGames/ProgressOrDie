8a03d221b1a8610b81dd5ee8e033a39babdc847d
TheAxeOfJustice <mandalorian.beskar@gmail.com>
2017-01-13 11:56:04 -0500

    Revert "Analytics Stuff"
    This reverts commit 34bca420e1b2bc6d00ccd664406318dd2d8a1de1.

--- Assets/Mixpanel/MixpanelExample.cs
+++ Assets/Mixpanel/MixpanelExample.cs
@@ -1,9 +1,8 @@
 ﻿
 using System.Collections.Generic;
 using UnityEngine;
-using mixpanel;
 
-public class MixpanelExample2 : MonoBehaviour
+public class MixpanelExample : MonoBehaviour
 {
 	public string Token;
 	public string DistinctID;
@@ -14,14 +13,14 @@ public class MixpanelExample2 : MonoBehaviour
 
 	public void Start()
 	{
-	//	Mixpanel.Token = Token;
-		//Mixpanel.DistinctID = DistinctID;
+		Mixpanel.Token = Token;
+		Mixpanel.DistinctID = DistinctID;
 
 		// Set some "super properties" to be sent with every event.
-		//Mixpanel.SuperProperties.Add("platform", Application.platform.ToString());
-		//Mixpanel.SuperProperties.Add("quality", QualitySettings.names[QualitySettings.GetQualityLevel()]);
-		//Mixpanel.SuperProperties.Add("fullscreen", Screen.fullScreen);
-		//Mixpanel.SuperProperties.Add("resolution", Screen.width + "x" + Screen.height);
+		Mixpanel.SuperProperties.Add("platform", Application.platform.ToString());
+		Mixpanel.SuperProperties.Add("quality", QualitySettings.names[QualitySettings.GetQualityLevel()]);
+		Mixpanel.SuperProperties.Add("fullscreen", Screen.fullScreen);
+		Mixpanel.SuperProperties.Add("resolution", Screen.width + "x" + Screen.height);
 	}
 
 	public void OnGUI()
@@ -29,37 +28,37 @@ public class MixpanelExample2 : MonoBehaviour
 		GUILayout.Label("This is an example demonstrating how to use the Mixpanel integration plugin for Unity3D.");
 		GUILayout.Label("All source code for this example is located in \"Assets/Mixpanel Analytics/MixpanelExample.cs\".");
 
-		//if(string.IsNullOrEmpty(Mixpanel.Token))
-	//	{
-	//		GUI.color = Color.red;
-	//		GUILayout.Label("Step 1: Set the Token property on the 'Mixpanel Example' object to your unique Mixpanel token string.");
-	//	}
-	
-	//	if(string.IsNullOrEmpty(Mixpanel.Token))
-	//		return;
-		
-//		GUILayout.BeginHorizontal();
-//		GUILayout.Label("Event Name:");
-//		_eventName = GUILayout.TextField(_eventName);
-//		GUILayout.EndHorizontal();
-	
-	//	GUILayout.BeginHorizontal();
-	//	GUILayout.Label("Property 1:");
-	//	_property1 = GUILayout.TextField(_property1);
-	//	GUILayout.EndHorizontal();
-	
-	//	GUILayout.BeginHorizontal();
-	//	GUILayout.Label("Property 2:");
-	//	_property2 = GUILayout.TextField(_property2);
-	//	GUILayout.EndHorizontal();
-	
-	//	if(GUILayout.Button("Send Event"))
+		if(string.IsNullOrEmpty(Mixpanel.Token))
 		{
-		//	Mixpanel.SendEvent(_eventName, new Dictionary<string, object>
-		//	{
-		//		{"property1", _property1},
-		//		{"property2", _property2},
-		//	});
+			GUI.color = Color.red;
+			GUILayout.Label("Step 1: Set the Token property on the 'Mixpanel Example' object to your unique Mixpanel token string.");
+		}
+	
+		if(string.IsNullOrEmpty(Mixpanel.Token))
+			return;
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Event Name:");
+		_eventName = GUILayout.TextField(_eventName);
+		GUILayout.EndHorizontal();
+	
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Property 1:");
+		_property1 = GUILayout.TextField(_property1);
+		GUILayout.EndHorizontal();
+	
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Property 2:");
+		_property2 = GUILayout.TextField(_property2);
+		GUILayout.EndHorizontal();
+	
+		if(GUILayout.Button("Send Event"))
+		{
+			Mixpanel.SendEvent(_eventName, new Dictionary<string, object>
+			{
+				{"property1", _property1},
+				{"property2", _property2},
+			});
 		}
 	}
 }

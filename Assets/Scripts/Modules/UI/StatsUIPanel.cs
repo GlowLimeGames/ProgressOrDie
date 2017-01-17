879cc35e0c97fdffc6284db59898abc759cbfaa3
TheAxeOfJustice <mandalorian.beskar@gmail.com>
2017-01-17 15:20:15 -0500

    Added Response from Buttons

--- Assets/Scripts/Modules/UI/StatsUIPanel.cs
+++ Assets/Scripts/Modules/UI/StatsUIPanel.cs
@@ -4,7 +4,64 @@
  * Usage: [no notes]
  */
 
-public class StatsUIPanel : UIElement 
-{	
+public class StatsUIPanel : UIElement
+	
+{
+	int AvailablePoint = 50;
+	int Body = 0;
+	int Mind = 0;
+	int Muscle = 0;
+	int Spirit = 0;
+	int Sp = 0;
 
+	public void BodyPlusClick()
+	{
+		Body = Body + 1;
+		print (Body);
+	}
+	public void BodyMinusClick()
+	{
+		Body = Body - 1;
+		print (Body);
+	}
+	public void MindPlusClick()
+	{
+		Mind = Mind + 1;
+		print (Mind);
+	}
+	public void MindMinusClick()
+	{
+		Mind = Mind - 1;
+		print (Mind);
+	}
+	public void MusclePlusClick()
+	{
+		Muscle = Muscle + 1;
+		print (Muscle);
+	}
+	public void MuscleMinusClick()
+	{
+		Muscle = Muscle - 1;
+		print (Muscle);
+	}
+	public void SpiritPlusClick()
+	{
+		Spirit = Spirit + 1;
+		print ("Spirit+1");
+	}
+	public void SpiritMinusClick()
+	{
+		Spirit = Spirit - 1;
+		print ("Spirit-1");
+	}
+	public void SpPlusClick()
+	{
+		Sp = Sp + 1;
+		print ("Sp+1");
+	}
+	public void SpMinusClick()
+	{
+		Sp = Sp - 1;
+		print ("Sp-1");
+	}
 }

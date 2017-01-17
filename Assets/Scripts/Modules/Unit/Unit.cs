a2073cb9579d9e54a204ab25843c3327e4922310
Isaiah Mann <contact@isaiahmann.com>
2017-01-17 14:12:58 -0500

    Removing unused interface methods

--- Assets/Scripts/Modules/Unit/Unit.cs
+++ Assets/Scripts/Modules/Unit/Unit.cs
@@ -129,22 +129,6 @@ public abstract class Unit : IUnit
 			return false;
 		}
 	}
-		
-	public bool CanMoveTo (IMapTile tile) {
-		throw new System.NotImplementedException();
-	}
-
-	public bool CanAttack (IUnit unit, AttackType attack) {
-		throw new System.NotImplementedException();
-	}
-
-	public bool CanMeleeAttack (IUnit unit) {
-		throw new System.NotImplementedException();
-	}
-
-	public bool CanMagicAttack (IUnit unit) {
-		throw new System.NotImplementedException();
-	}
 
 	public void MoveTo (IMapTile tile) {
 		throw new System.NotImplementedException();
